68a7d5f81ef4303f8cd7743c264a7fbb949463bf
Jesus Antonio Garcia Fusi <83846459+SilentCry1234@users.noreply.github.com>
2023-02-05 12:53:07 -0300

    Update
    A Stamina bar was introduce

--- 2.Scripts/LevelManager.gd
+++ 2.Scripts/LevelManager.gd
@@ -2,26 +2,35 @@ extends Node
 
 
 var score = 0
-var stamina = 15
+var stamina = 100
 var timeleft = 50
+
+var barraStamina
 
 var paused = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	barraStamina = get_tree().get_nodes_in_group("StaminaBar")[0]
 	
 	get_parent().get_node("UserInterface/GameOverPanel").hide()
+	
 	while true:
 		yield(get_tree().create_timer(1), "timeout")
 		score +=1
 		timeleft -= 1
 		if stamina < 100:
 			stamina += 5
-
+	
 
 func _process(delta):
 	get_parent().get_node("UserInterface/CanvasLayer/RichTextLabel").text = "Time without starving: " + str(score)+"\nBudget: " + str(timeleft) + "\nStamina: " + str(stamina)
+	update_StaminaBar()
 	gameover()
+
+func update_StaminaBar():
+	barraStamina.value = stamina
+
 
 func gameover():
 	if stamina <= 0 or timeleft <= 0:
